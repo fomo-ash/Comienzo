5d7f441a8f697ddd01e1ae27c2bafd74c3a38421
13thAline <sahoosailen13@gmail.com>
2025-11-13 18:21:42 +0530

    my checkpoint

--- frontend/freshers/src/App.jsx
+++ frontend/freshers/src/App.jsx
@@ -1,26 +1,34 @@
-import { useState } from "react";
-import EntryAnimation from "./components/intro";
+import React, { useState } from "react";
+import NavBar from "./components/navbar";
 import Hero from "./components/Hero";
-import MusicPlayer from "./components/music";
+import Features from "./components/Features";
+import HighlightsGrid from "./components/HighlightsGrid";
+import Footer from "./components/Footer";
+import EntryAnimation from "./components/intro";
 
 export default function App() {
   const [showEntry, setShowEntry] = useState(true);
-
   return (
     <div className="relative min-h-screen overflow-hidden">
-      
-      
-      {showEntry && (
-        <EntryAnimation onFinish={() => setShowEntry(false)} />
-      )}
-
-     
-      {!showEntry && (
-        <>
+          
+          
+          {showEntry && (
+            <EntryAnimation onFinish={() => setShowEntry(false)} />
+          )}
+    
+         
+          {!showEntry && (
+              <div className="min-h-screen w-full bg-[#9ccfcf] font-sans text-gray-800">
+          <NavBar />
           <Hero />
-          <MusicPlayer/>    
-        </>
-      )}
+          <div className="max-w-screen-2xl mx-auto px-8 lg:px-16">
+            <Features />
+            <HighlightsGrid />
+          </div>
+          <Footer />
+        </div>
+    
+          )}
     </div>
   );
 }
