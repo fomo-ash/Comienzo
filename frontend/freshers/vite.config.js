addd9189c5201d476e183dcb7c4d6dbe0382c745
fomo-ash <ashutoshbadapanda02@gmail.com>
2025-11-19 00:02:22 +0530

    please

--- frontend/freshers/vite.config.js
+++ frontend/freshers/vite.config.js
@@ -4,5 +4,8 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  css: {
+    transformer: 'postcss' // disable LightningCSS
+  },
   plugins: [react(), tailwindcss()],
 })
